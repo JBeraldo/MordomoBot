135a0ade16f102d96c5b84ec7d3e5a5857779087
JBeraldo <joaoangelo409@gmail.com>
2021-06-26 18:25:52 -0300

    Deteçao do comando add

--- Bot.js
+++ Bot.js
@@ -2,28 +2,32 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 const dotenv = require('dotenv');
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+function conectar(){//conection factory
+  const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     database : 'ADS'
-});
-// connect to mysql
-connection.connect(function(err) {
+  });
+  // connect to mysql
+  connection.connect(function(err) {
     // in case of error
-    if(err){
+      if(err){
         console.log(err.code);
         console.log(err.fatal);
     }
 });
-$query = 'CREATE TABLE teste(PersonID int)'
-connection.query($query, function(err, rows, fields) {
+return connection
+}
+const db=conectar(); //variavel que recebe a conexão com o MySql
+/*$query = 'CREATE TABLE teste(PersonID int)'
+db.query($query, function(err, rows, fields) {
     if(err){
         console.log("An error ocurred performing the query.");
         return;
     }
 
     console.log("Query succesfully executed: ", rows);
-});
+});*/
 
 dotenv.config();
 
@@ -36,4 +40,46 @@ client.on("message", msg => {
     msg.reply("pong");
   }
 })
+
+client.on("message", msg => {
+  if (msg.content.startsWith("?")) {
+    let mensagem = msg.content.split(" ");
+    for (let i = 0; i < mensagem.length; i++) {
+      mensagem[i]=mensagem[i].toLowerCase();
+    }
+    switch (mensagem[0]) {
+      case "?add" :
+        if(mensagem[1] !== undefined){
+          switch (mensagem[1]) {
+            case "materia" :
+            case "matéria" :
+              msg.channel.send("Adicionou materia")
+              break;
+            case "trabalho" :
+              msg.channel.send("Adicionou Trabalho")
+              break;
+            case "aula" :
+              msg.channel.send("Adicionou Aula")
+              break;
+          }
+        }
+        else{
+          msg.channel.send("Erro de sintaxe BURRO")
+        }        
+        break;
+      case "?del" :
+        msg.channel.send("deu certo");
+        break;
+      case "?hoje" :
+        msg.channel.send("deu certo");
+        break;
+      case "?semana" :
+        msg.channel.send("deu certo");
+        break;
+    }
+    
+  }
+})
+
+
 client.login(process.env.TOKEN)
